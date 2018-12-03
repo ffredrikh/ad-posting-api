45d3b3f49b099863764707079fc9cef7488cb7ba
Fredrik <fredrik.hobert@outlook.com>
2018-12-03 08:28:00 +0100

    Initial commit

---
+++ AdPostingApi/Entities/AdInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdPostingApi.Entities
+{
+    public class AdInfo
+    {
+    }
+}
---
+++ AdPostingApi/Models/AdInfoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdPostingApi.Models
+{
+    public class AdInfoDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Category { get; set; }
+
+    }
+}
